e8c8bd6cf0baa060c70d125dfdff1205171744e8
Derek Jobst <git@derekjobst.com>
2017-04-16 15:24:01 -0400

    Added frontend with templating

--- track_server/app.js
+++ track_server/app.js
@@ -2,14 +2,35 @@ var express = require('express');
 var bodyParser = require('body-parser')
 
 var app = express();
+app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
-app.get('/track', function (req, res) {
-	res.render('track_test');
+app.get('/dashboard', function (req, res) {
+
+	var data = {
+		goal: 100,
+		consumption: 43,
+		events: [
+			{
+				time: "10:03:34 01/04/2012",
+				weight: 456
+			},
+			{
+				time: "11:33:56 01/04/2012",
+				weight: 345
+			},
+			{
+				time: "12:01:00 01/04/2012",
+				weight: 234
+			}
+		]
+	}
+
+	res.render('dashboard', data);
 });
 
 app.get('/setWeight', function (req, res) {
